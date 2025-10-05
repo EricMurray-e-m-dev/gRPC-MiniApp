8b2ac07da51f1b07dd9a05d488d0e40315aa80f9
e-m-dev <eric.murray.dev@gmail.com>
2025-10-05 12:05:35 +0100

    fix: correct grpc connection method from Dial to NewClient

--- client/main.go
+++ client/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	// Connect to the server
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
